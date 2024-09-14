c914f7f9a3e6a93b055adf47f5c275e901ab4018
Alexandru Fikl <alexfikl@gmail.com>
2024-09-14 20:21:27 +0300

    feat: update script

--- scripts/run-exhibits.sh
+++ scripts/run-exhibits.sh
@@ -7,25 +7,15 @@ set -Eeuo pipefail
 
 function with_echo() {
     echo "+++" "$@"
-    "$@"
+    # "$@"
 }
 
-infile=${1}
-index_start=${2:-0}
-index_end=${3:-${index_start}}
-suffix=$(basename "${infile}" .npz)
+suffix=$(date "+%Y%m%d-%H%M%S")
 
-for i in $(seq -f '%02g' ${index_start} ${index_end}); do
-    with_echo python scripts/generate-matrix-gallery.py \
-        --overwrite --quiet \
-        --max-escape-radius 40 \
-        --ranges "${i}" \
-        --suffix "${suffix}" \
-        --outfile 'src/gallery.rs' \
-        --infile "${infile}"
-    with_echo cargo fmt -- 'src/gallery.rs'
-    sed -i "64s/gallery::EXHIBIT_.*/gallery::EXHIBIT_${i}_${suffix^^};/" 'src/main.rs'
-
-    with_echo cargo build --release
-    with_echo ./target/release/netbrot -r 1200 -- "result-${suffix,,}-${i}.png"
+for filename in $@; do
+    with_echo ./target/release/netbrot \
+        --resolution 1200 \
+        --maxit 512 \
+        --outfile "${filename%.json}-${suffix}.png" \
+        "${filename}"
 done
